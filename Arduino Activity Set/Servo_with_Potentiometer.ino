b6bd3db265d968287ca0047bc925b62be8177f57
Nicholas Vadivelu <nick@bassoft.com>
2017-04-06 19:26:42 -0400

    Finished Servo w/ Potentiometer

---
+++ Arduino Activity Set/Servo_with_Potentiometer.ino
@@ -0,0 +1,25 @@
+/*  Nicholas Vadivelu
+ *  TER4M1
+ *  Mr. Jay
+ *  3 April 2017
+ *  
+ *  Arduino Servo with Potentiometer Activity
+ */
+
+#include <Servo.h>
+
+Servo servo;
+
+int pot = 4; //potentiometer pin
+int value; //variable to be read from pot
+
+void setup() {
+  servo.attach(9); //attaches pin 9 to servo
+}
+
+void loop() {
+  value = analogRead(pot); //reads servo value
+  servo.write(map(value, 0, 1023, 0, 180)); //proportionally scales and sets servo to position
+  delay(15); //waits for servo
+}
+
