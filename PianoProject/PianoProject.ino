e2f8c6e5e8a1e187993123df2d564b3f8991221b
Nicholas Vadivelu <nick@bassoft.com>
2017-04-11 23:13:28 -0400

    implemented debounce for buttons

--- PianoProject/PianoProject.ino
+++ PianoProject/PianoProject.ino
@@ -10,11 +10,18 @@
 //Pins used on Arduino
 int keyPins[] = {2, 3, 4, 5, 6, 7, 8, 9}; //piano key pins
 int recordPin = 11, playPin = 12, speakerPin = 13; //pins
-
-//Other required variables
 int numPins = 8; //number of pins to make array iteration easier
+
+//Notes/Tones constants
 char notes[] = "cdefgabC "; //notes available on the piano
 int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 }; //frequencies that correspond with the notes
+
+//Debounce Variables
+int prevStates[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW}; //stores the previous state of the pins
+long lastDebounceTime[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //stores the time a particular button was pressed
+int debounceDelay = 50; //debounce time, make it higher if flickers, lower if not sensitive enough
+
+//controls playback/playing of notes
 bool recording = false; //controls it he arduino is recoridng or not
 bool playing = false; //controls if the song is playing
 long startTime = 0; //subtracted from the all the recorded times to prevent overflow
@@ -30,6 +37,7 @@ void playTone(int tone, int duration) { //plays a tone
   }
 }
 
+/* Unused; tones are always played. 
 void playNote(char note, int duration) { //plays notes
   char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
   int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
@@ -40,14 +48,26 @@ void playNote(char note, int duration) { //plays notes
       playTone(tones[i], duration);
     }
   }
-}
+}*/
 
+/* No Longer used (debounce was implemented)
 int getButton() { //gets which button was pressed, returns -1 if none were pressed
   for (int i = 0 ; i < numPins ; i++)
     if (digitalRead(keyPins[i]) == HIGH) return i;
   if (digitalRead(recordPin) == HIGH) return recordPin;
   else if(digitalRead(playPin) == HIGH) return playPin;
   else return -1;
+}*/ 
+
+int readBtn(int pin) { //reads button with debounce
+  int val = digitalRead(pin); //reads input from pin
+
+  if(val != prevStates[pin]) lastDebounceTime[pin] = millis(); //stores the last time the button was pressed.
+
+  if ((millis() - lastDebounceTime[pin]) > debounceDelay) //if the button was held long enough, return high
+    return HIGH;
+  else
+    return LOW;
 }
 
 void setup() {
@@ -60,10 +80,8 @@ void setup() {
 }
 
 void loop() {
-  int btnPressed = getButton(); //gets the button that was pressed
-
   //Initialize recording sequence
-  if(btnPressed == recordPin){
+  if(readBtn(recordPin) == HIGH){
     recording = !recording; //turn off if on, on if off
 
     if (recording) { //if it just started recording, reset variables
@@ -75,7 +93,7 @@ void loop() {
   }
 
   //Do playing sequence
-  else if(btnPressed == playPin){
+  else if(readBtn(playPin) == HIGH){
     for (int i = 0 ; i < noteCounter ; i++){ //go through all the notes
       if (recordedTones[i] != 0) { //play the note or pause
         playTone(recordedTones[i], recordedTimes[i+1]-recordedTimes[i]);
@@ -89,17 +107,19 @@ void loop() {
   else {
     boolean keyPressed = false; //if a key wasn't pressed, need to record pause
     for (int i = 0 ; i < numPins ; i++) {
-      if (btnPressed == keyPins[i]) {
+      if (readBtn(keyPins[i]) == HIGH) { //if the current pin button is being pressed
         keyPressed = true; 
         if (recording){ //vector add note and add time to the vector
           recordedTones[noteCounter] = tones[i];
           recordedTimes[noteCounter++] = (millis()-startTime);
         }
-        playNote(notes[i], 1);//fix this
-      }      
+        while (readBtn(keyPins[i]) == HIGH) { //play note while the button is held down
+          playNote(notes[i], 15);
+        }
+      }
     }
 
-    if (!keyPressed && recording){
+    if (!keyPressed && recording){ //records pauses
       recordedTones[noteCounter] = 0; //adds pause
       recordedTimes[noteCounter++] = (millis()-startTime);
     }
